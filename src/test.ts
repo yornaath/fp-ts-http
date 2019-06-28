8f1f0bc9ddc88ee2ea6c34da07db20bfd5245d9c
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2019-06-28 23:55:42 +0200

    werk

--- src/test.ts
+++ src/test.ts
@@ -7,24 +7,34 @@ let server = createServer()
 
 const userById = lit('users').then(int("userid"))
 
-server = get<{userid: number}, string>(server, userById.then(end), async(req) => {
-  return {
-    status: 200,
-    headers: {},
-    body: "get success: " + req.path.userid
-  }
-})
+const getUserById = get<{userid: number}, string>(
+  server, 
+  userById.then(end), 
+  async(req) => {
+    return {
+      status: 200,
+      headers: {},
+      body: "get success: " + req.path.userid
+    }
+  })
 
 const userMessages = userById.then(lit("messages"))
 const userMessage = t.type({ message: t.string })
 
-server = post<{userid: number}, {message: string}, string>(server, userMessages.then(end), userMessage, async(req) => {
-  return {
-    status: 200,
-    headers: {},
-    body: `post message "${req.body.message}" to user ${req.path.userid}`
-  }
-})
+const postMessageToUserById = server = post<{userid: number}, {message: string}, string>(
+  server, 
+  userMessages.then(end), 
+  userMessage, async(req) => {
+    return {
+      status: 200,
+      headers: {},
+      body: `post message "${req.body.message}" to user ${req.path.userid}`
+    }
+  })
 
-driver(server, 3000).run()
+driver({
+  ...server,
+  ...getUserById,
+  ...postMessageToUserById
+}, 3000).run()
   .then(() => console.log("server running"))
